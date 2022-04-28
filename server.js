f4cce420deaf9661d979441075f052a5a8364e62
RodrigoEC <rodrigo.cavalcanti@ccc.ufcg.edu.br>
2022-04-28 00:33:43 -0300

    update serve.js

--- server.js
+++ server.js
@@ -19,12 +19,7 @@ app.all('*', function (req, res, next) {
         // CORS Preflight
         res.send();
     } else {
-        var targetURL = req.header('Target-URL', 'https://api.notion.com/v1/');
-        if (!targetURL) {
-            res.send(500, { error: 'There is no Target-Endpoint header in the request' });
-            return;
-        }
-        request({ url: targetURL + req.url, method: req.method, json: req.body, headers: {'Authorization': req.header('Authorization')} },
+        request({ url: req.url.substring(1,), method: req.method, json: req.body, headers: {'Authorization': req.header('Authorization'), 'Notion-Version': req.header('Notion-Version')} },
             function (error, response, body) {
                 if (error) {
                     console.error('error: ' + response.statusCode)
